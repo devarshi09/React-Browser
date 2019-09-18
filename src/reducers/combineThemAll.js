474cc4e4843939a4ddde3e30de2f697a1832302e
Devarshi Khanna <devarshikhanna@MTPLs-MacBook-Pro.local>
2019-09-19 00:44:45 +0530

    removed few folders

--- src/reducers/combineThemAll.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as reduxFormReducer } from 'redux-form';
-
-
-
-export default combineReducers({
-  form: reduxFormReducer, // mounted under "form"
-});
